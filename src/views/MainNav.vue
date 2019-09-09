2907de6e638ccda8a2ab66dacb11e5752de88eb3
lying <610398531@qq.com>
2019-09-10 00:43:00 +0800

    新增用户以及form组件值清空

--- src/views/MainNav.vue
+++ src/views/MainNav.vue
@@ -26,15 +26,15 @@
                 @close="handleClose">
                 <el-menu-item index="2">
                   <i class="el-icon-menu"></i>
-                  <span slot="title">导航一</span>
+                  <span slot="title">商户管理</span>
                 </el-menu-item>
                 <el-menu-item index="3" >
                   <i class="el-icon-document"></i>
-                  <span slot="title">导航二</span>
+                  <span slot="title">电子票据</span>
                 </el-menu-item>
                 <el-menu-item index="4">
                   <i class="el-icon-location"></i>
-                  <span slot="title">导航三</span>
+                  <span slot="title">库存管理</span>
                 </el-menu-item>
                 <el-submenu index="1">
                   <template slot="title">
