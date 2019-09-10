86a7af93aa86338cfa8e4e33247d84371a6348ec
610398531@qq.com <lyb19961016>
2019-09-10 15:44:46 +0800

    修改引用UI组件和APP的先后顺序 防止组件的样式没有被引用

--- src/main.js
+++ src/main.js
@@ -1,11 +1,12 @@
 import Vue from 'vue'
-import App from './App'
 import VueRouter from 'vue-router'
 import router from './router'
 import axios from 'axios'
 
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
+import App from './App'
+
 
 Vue.config.productionTip = false;
 Vue.prototype.$ajax=axios;
